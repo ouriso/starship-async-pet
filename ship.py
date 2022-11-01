d317b6c7222089f8f495ad0755023482ba4cf9ec
Gumerov Artur <2diver7@gmail.com>
2022-11-02 01:15:24 +0300

    Experiments with sleep timer

--- ship.py
+++ ship.py
@@ -1,32 +1,36 @@
-import asyncio
 from itertools import cycle
-from frames import draw_frame, get_frame_size  #, read_controls
+
+from frames import draw_frame, get_frame_size  # , read_controls
+from utils.sleep import do_sleep
 
 with open(r'./animations/ship_frame_1.txt', 'r', encoding='utf-8') as fp:
-  ship_frame_1 = fp.read()
-  rows_1, columns_1 = get_frame_size(ship_frame_1)
-  square_1 = rows_1 * columns_1
+    ship_frame_1 = fp.read()
+    rows_1, columns_1 = get_frame_size(ship_frame_1)
+    square_1 = rows_1 * columns_1
 
 with open(r'./animations/ship_frame_2.txt', 'r', encoding='utf-8') as fp:
-  ship_frame_2 = fp.read()
-  rows_2, columns_2 = get_frame_size(ship_frame_2)
-  square_2 = rows_2 * columns_2
+    ship_frame_2 = fp.read()
+    rows_2, columns_2 = get_frame_size(ship_frame_2)
+    square_2 = rows_2 * columns_2
 
 with open(r'./animations/ship.txt', 'r', encoding='utf-8') as fp:
-  ship = fp.read()
-  ship_rows, ship_columns = get_frame_size(ship_frame_2)
+    ship = fp.read()
+    ship_rows, ship_columns = get_frame_size(ship_frame_2)
 
 ship_frames = [
-  (ship_frame_1, False),
-  (ship_frame_1, True),
-  (ship_frame_2, False),
-  (ship_frame_2, True)
+    (ship_frame_1, False),
+    (ship_frame_1, True),
+    (ship_frame_2, False),
+    (ship_frame_2, True)
 ]
 
 
 async def ship_animate(canvas, ship_y, ship_x):
-
-  for ship_frame, negative in cycle(ship_frames):
-    draw_frame(canvas, ship_y, ship_x, ship)
-    draw_frame(canvas, ship_y + ship_rows + rows_1, ship_x, ship_frame, negative)
-    await asyncio.sleep(0)
+    for ship_frame, negative in cycle(ship_frames):
+        draw_frame(canvas, ship_y, ship_x, ship)
+        draw_frame(
+            canvas, ship_y + ship_rows + rows_1, ship_x, ship_frame, negative
+        )
+        clock = do_sleep(1)
+        await clock
+        # await asyncio.sleep(0)
--- stars.py
+++ stars.py
@@ -5,26 +5,26 @@ from utils.sleep import sleep_ticks
 
 stars_symbols = ['*', ':', '.', '+']
 stars_stages = (
-  (10, curses.A_DIM),
-  (3, None),
-  (5, curses.A_BOLD),
-  (3, None)
+    (10, curses.A_DIM),
+    (3, None),
+    (5, curses.A_BOLD),
+    (3, None)
 )
 
 
 async def blink(canvas, row, column, delay, symbol='*'):
-  symbol = choice(stars_symbols)
-  await sleep_ticks(delay)
-  while True:
-    for ticks, style in stars_stages:
-      await sleep_ticks(ticks)
-      canvas.addstr(row, column, symbol, style or curses.A_NORMAL)
+    symbol = choice(stars_symbols)
+    await sleep_ticks(delay)
+    while True:
+        for ticks, style in stars_stages:
+            await sleep_ticks(ticks)
+            canvas.addstr(row, column, symbol, style or curses.A_NORMAL)
 
 
 def generate_stars(max_y: int, max_x: int, stars_number: int = 50) -> set:
-  stars = set()
-  for _ in range(stars_number):
-    stars.add(
-      (randint(1, max_y - 2), randint(1, max_x - 2))
-    )
-  return stars
+    stars = set()
+    for _ in range(stars_number):
+        stars.add(
+            (randint(1, max_y - 2), randint(1, max_x - 2))
+        )
+    return stars
