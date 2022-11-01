0d94e66afb81c93b4967ca3db9591c27801a0e2e
Gumerov Artur <2diver7@gmail.com>
2022-11-01 20:22:49 +0000

    Stationary starship with blinking stars

---
+++ gun.py
@@ -0,0 +1,41 @@
+import asyncio
+import curses
+
+
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    row_col_div = abs(rows_speed / columns_speed)
+
+    if columns_speed == 0 or row_col_div > 1.6:
+      symbol = '|'
+    elif rows_speed == 0 or row_col_div < 0.6:
+      symbol = '-'
+    elif (rows_speed * columns_speed) > 0:
+      symbol = '\\'
+    else:
+      symbol = '/'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
--- main.py
+++ main.py
@@ -1,84 +1,50 @@
-import asyncio
 import time
 import curses
-from random import randint, choice
+from random import randint, randrange
+from gun import fire
+from ship import ship_animate
+from stars import blink, generate_stars
 
+with open(r'./animations/ship_frame_1.txt', 'r', encoding='utf-8') as fp:
+  ship_frame_1 = fp.read()
 
-# stars = (
-#   (3, 7),
-#   (7, 6),
-#   (4, 11),
-#   (2, 15),
-#   (10, 8),
-#   (5, 3)
-# )
-stars = set()
-stars_symbols = ['*', ':', '.', '+']
-
-
-class EventLoop:
-  def __await__(self):
-    return (yield self)
-
-
-class Sleep(EventLoop):
-  def __init__(self, seconds):
-    self.seconds = seconds
-
-
-async def do_sleep(times_count=1):
-  for _ in range(times_count):
-    await Sleep(0.1)
-
-
-async def change_star_stage(times_count):
-  ticks = do_sleep(times_count)
-  await ticks
-
-
-async def blink(canvas, row, column, symbol='*'):
-  symbol = choice(stars_symbols)
-  while True:
-    canvas.addstr(row, column, symbol, curses.A_DIM)
-    await change_star_stage(10)
-  
-    canvas.addstr(row, column, symbol)
-    await change_star_stage(3)
-  
-    canvas.addstr(row, column, symbol, curses.A_BOLD)
-    await change_star_stage(5)
-  
-    canvas.addstr(row, column, symbol)
-    await change_star_stage(3)
+with open(r'./animations/ship_frame_2.txt', 'r', encoding='utf-8') as fp:
+  ship_frame_2 = fp.read()
+ship_frames = [ship_frame_1, ship_frame_2]
 
 
 def draw(canvas):
   curses.curs_set(False)
   max_y, max_x = curses.window.getmaxyx(canvas)
-  for _ in range(100):
-    stars.add((randint(1, max_y-1), randint(1, max_x-1)))
+
+  start_pos = (
+    max_y * 3 / 4, max_x / 2
+  )
+  
+  stars = generate_stars(max_y, max_x)
 
   canvas.border()
   canvas.refresh()
-  corutines = [blink(canvas, row, column) for row, column in stars]
+  corutines = [
+    fire(
+      canvas, start_pos[0] - 1, start_pos[1] + 2, 
+      randrange(-10, 10, 1) / 10, randrange(-10, 10, 1) / 10
+    )
+  ]
+  corutines_add = [blink(canvas, row, column, randint(10, 70)) for row, column in stars]
+  corutines_add.append(ship_animate(canvas, *start_pos))
 
   while True:
-    for corutine in corutines:
-      corutine.send(None)
-      canvas.refresh()
-      # time.sleep(0.1)
-      corutine.send(None)
-      canvas.refresh()
-      # time.sleep(0.1)
-      corutine.send(None)
-      canvas.refresh()
-      # time.sleep(1)
-      corutine.send(None)
-      canvas.refresh()
-      # time.sleep(1)
-      corutine.send(None)
-      canvas.refresh()
-    time.sleep(1)
+    corutines.extend(corutines_add)
+    for corutine in corutines.copy():
+      try:
+        corutine.send(None)
+        canvas.refresh()
+      except StopIteration:
+        corutines.remove(corutine)
+        continue
+    
+    time.sleep(0.2)
 
 
 if __name__ == '__main__':
