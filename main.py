1a5372e8a8233e0eb5066b8201fd3759a55b2886
Gumerov Artur <2diver7@gmail.com>
2022-11-09 00:35:04 +0300

    Moving and blinking coroutines works as they should

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import time
 import curses
+import time
 from itertools import cycle
 from random import randint, randrange
 
+from config import STARS_NUMBER, BASE_DELAY
 from controls import read_controls
 from gun import fire
 from ship import ship_animate, offsets_calc
-from stars import blink, generate_stars
+from stars import generate_stars, star
 
 with open(r'./animations/ship_frame_1.txt', 'r', encoding='utf-8') as fp:
     ship_frame_1 = fp.read()
@@ -23,46 +24,67 @@ def draw(canvas):
     start_y, start_x = (
         max_y * 3 / 4, max_x / 2
     )
-    need_fire = False
 
-    stars = generate_stars(max_y, max_x)
+    stars = generate_stars(max_y, max_x, STARS_NUMBER)
 
     canvas.nodelay(True)
 
-    corutines_add = [
-        blink(canvas, row, column, randint(10, 30)) for row, column in stars
+    coroutines_stars = [
+        [0, star(canvas, row, column, randint(1, 3), symbol)]
+        for row, column, symbol in stars
     ]
 
-    corutines = []
+    frames = cycle(ship_frames)
+    need_fire = False
 
-    for ship_frame in cycle(ship_frames):
-        canvas.border()
-        corutines.extend(corutines_add)
-        corutines.append(ship_animate(canvas, start_y, start_x, ship_frame))
-        if need_fire:
-            corutines.append(
-                fire(
-                    canvas, start_y - 1, start_x + 2,
-                    randrange(-10, 0, 1) / 10,
-                    randrange(-10, 10, 1) / 10
-                )
+    while True:
+        sleeping_coroutines = [
+            *coroutines_stars
+        ]
+
+        while sleeping_coroutines:
+            canvas.border()
+            ship_frame = next(frames)
+
+            sleeping_coroutines = [
+                [timeout - BASE_DELAY, coroutine] for timeout, coroutine in sleeping_coroutines]
+            time.sleep(BASE_DELAY)
+
+            # делим бомбы на активные и спящие
+            active_coroutines = [
+                [timeout, coroutine] for timeout, coroutine in sleeping_coroutines if timeout <= 0]
+            sleeping_coroutines = [
+                [timeout, coroutine] for timeout, coroutine in sleeping_coroutines if timeout > 0]
+
+            active_coroutines.append(
+                [0, ship_animate(canvas, start_y, start_x, ship_frame)]
             )
-        for corutine in corutines.copy():
-            try:
-                corutine.send(None)
-                canvas.refresh()
-            except StopIteration:
-                corutines.remove(corutine)
-                continue
+            if need_fire:
+                active_coroutines.append(
+                    [0, fire(
+                        canvas, start_y, start_x + 2,
+                        randrange(-10, 0, 1) / 10,
+                        randrange(-10, 10, 1) / 10
+                    )]
+                )
 
-        y_offset, x_offset, need_fire = read_controls(canvas)
-        y_offset, x_offset = offsets_calc(
-            canvas, ship_frame, start_y, start_x, y_offset, x_offset
-        )
-        start_y += y_offset
-        start_x += x_offset
+            for _, coroutine in active_coroutines:
+                try:
+                    sleep_command = coroutine.send(None)
+                    canvas.refresh()
+                except StopIteration:
+                    continue
+                seconds_to_sleep = sleep_command.seconds
+                sleeping_coroutines.append([seconds_to_sleep, coroutine])
 
-        time.sleep(0.1)
+            y_offset, x_offset, need_fire = read_controls(canvas)
+            y_offset, x_offset = offsets_calc(
+                canvas, ship_frame, start_y, start_x, y_offset, x_offset
+            )
+            start_y += y_offset
+            start_x += x_offset
+
+            time.sleep(BASE_DELAY)
 
 
 if __name__ == '__main__':
