99d724b0b86542bd9f0ddcad0544e10a02edeeeb
Gumerov Artur <2diver7@gmail.com>
2022-11-09 00:36:56 +0300

    Unused functions removed

--- utils/sleep.py
+++ utils/sleep.py
@@ -6,13 +6,3 @@ class EventLoop:
 class Sleep(EventLoop):
     def __init__(self, seconds: float):
         self.seconds = seconds
-
-
-async def do_sleep(times_count: int = 1):
-    for _ in range(times_count):
-        await Sleep(0.1)
-
-
-async def sleep_ticks(times_count: int):
-    ticks = do_sleep(times_count)
-    await ticks
