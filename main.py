4aa68eee02b2253e761c05c9312bed44ebafb40a
Gumerov Artur <2diver7@gmail.com>
2022-11-14 00:17:07 +0300

    Working stable event-loop version with SpaceObjects

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import curses
 import time
-from random import randint
 
-from config import STARS_NUMBER, BASE_DELAY
+from config import STARS_NUMBER
 from controls import read_controls
 from entities.star import generate_stars
 from gadgets.starship import StarShip
+from utils.canvas_params import set_border_params, get_border_params
 
 with open(r'./animations/ship_frame_1.txt', 'r', encoding='utf-8') as fp:
     ship_frame_1 = fp.read()
@@ -17,7 +17,8 @@ ship_frames = [ship_frame_1, ship_frame_1, ship_frame_2, ship_frame_2]
 
 def draw(canvas):
     curses.curs_set(False)
-    max_y, max_x = curses.window.getmaxyx(canvas)
+    set_border_params(canvas)
+    max_y, max_x = get_border_params()
 
     start_y, start_x = (
         round(max_y * 3 / 4), round(max_x / 2)
@@ -28,14 +29,26 @@ def draw(canvas):
 
     canvas.nodelay(True)
 
-    active_coroutines = [
-        [0, starship.animate(canvas)],
-        *[[randint(0, 5), star.animate(canvas)] for star in stars]
+    sleeping_coroutines = [
+        *[[0, star.animate(canvas)] for star in stars],
+        [0, starship.animate(canvas)]
     ]
-    sleeping_coroutines = []
 
     while True:
         canvas.border()
+
+        min_sleep, _ = min(sleeping_coroutines, key=lambda pair: pair[0])
+        sleeping_coroutines = [
+            [timeout - min_sleep, coroutine]
+            for timeout, coroutine in sleeping_coroutines]
+
+        # делим корутины на активные и спящие
+        active_coroutines = [
+            [timeout, coroutine]
+            for timeout, coroutine in sleeping_coroutines if timeout <= 0]
+        sleeping_coroutines = [
+            [timeout, coroutine]
+            for timeout, coroutine in sleeping_coroutines if timeout > 0]
 
         for _, coroutine in active_coroutines:
             try:
@@ -46,24 +59,13 @@ def draw(canvas):
             seconds_to_sleep = sleep_command.seconds
             sleeping_coroutines.append([seconds_to_sleep, coroutine])
 
-        sleeping_coroutines = [
-            [timeout - BASE_DELAY, coroutine]
-            for timeout, coroutine in sleeping_coroutines]
-
-        # делим бомбы на активные и спящие
-        active_coroutines = [
-            [timeout, coroutine]
-            for timeout, coroutine in sleeping_coroutines if timeout <= 0]
-        sleeping_coroutines = [
-            [timeout, coroutine]
-            for timeout, coroutine in sleeping_coroutines if timeout > 0]
-
         y_offset, x_offset, need_fire = read_controls(canvas)
-        starship.change_position(y_offset, x_offset)
+        if y_offset != 0 or x_offset != 0:
+            starship.change_position(y_offset, x_offset)
         if need_fire:
             sleeping_coroutines.append([0, starship.fire(canvas)])
 
-        time.sleep(BASE_DELAY)
+        time.sleep(min_sleep)
 
 
 if __name__ == '__main__':
