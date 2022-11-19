e5fe8c27a18686e751ae181ded5fe0a39741ec06
Gumerov Artur <2diver7@gmail.com>
2022-11-19 13:55:49 +0300

    Created function that calculate number of ticks to await expected number of seconds

--- utils/sleep.py
+++ utils/sleep.py
@@ -1,6 +1,22 @@
 import asyncio
 
+from config import BASE_DELAY
 
-async def sleep(ticks=1):
+
+async def sleep(ticks: int = 1) -> None:
+    """
+    Implementation of a custom eventloop.
+    :param ticks: number of ticks before coroutine exhaustion
+    :return:
+    """
     for _ in range(ticks):
         await asyncio.sleep(0)
+
+
+def calc_ticks_number(seconds_to_sleep: float) -> int:
+    """
+    Calculate the number of ticks before coroutine exhaustion.
+    :param seconds_to_sleep: expected seconds to sleep
+    :return: number of ticks depending on BASE_DELAY
+    """
+    return round(seconds_to_sleep / BASE_DELAY)
