8daa8d04e1f41da2ad4fa94b2a50167c5fcfb138
Gumerov Artur <2diver7@gmail.com>
2022-11-29 22:49:40 +0300

    Garbage explosion function is added

--- entities/garbage.py
+++ entities/garbage.py
@@ -4,18 +4,24 @@ from random import choice, randint
 from entities.obstacle import Obstacle
 from entities.space_objects import SpaceObject
 from utils.canvas_dimensions import get_canvas_dimensions
-from utils.event_loop import append_coroutine, get_obstacles
+from utils.event_loop import append_coroutine, get_obstacles, get_coroutines
 from utils.frames import draw_frame, get_frame_size, get_frames_from_files
 from utils.sleep import sleep
 
 
 class Garbage(SpaceObject):
+    explode_frames = get_frames_from_files(
+        r'./animations/explosion.txt'
+    ).split(',\n')
+
     async def animate(self, canvas) -> None:
         height, width = get_canvas_dimensions()
 
         while self.position_y < height:
             if self.need_to_stop:
-                self.position_y = height + 1
+                coroutines = get_coroutines()
+                coroutines.append(self.explode(canvas))
+                get_obstacles().remove(self)
                 return
             draw_frame(
                 canvas, self.position_y, self.position_x, self.frame
@@ -26,6 +32,23 @@ class Garbage(SpaceObject):
                 self.frame, negative=True
             )
             self.position_y += self.speed_by_y
+
+    async def explode(self, canvas) -> None:
+        explode_size = get_frame_size(self.explode_frames[0])
+        center_y = self.position_y + (
+                self.object_borders().bottom - self.object_borders().top) / 2
+        center_x = self.position_x + (
+                self.object_borders().right - self.object_borders().left) / 2
+        explode_y = center_y - explode_size.height / 2
+        explode_x = center_x - explode_size.width / 2
+        for frame in self.explode_frames:
+            draw_frame(
+                canvas, explode_y, explode_x, frame
+            )
+            await sleep(2)
+            draw_frame(
+                canvas, explode_y, explode_x, frame, negative=True
+            )
 
 
 async def generate_garbage(canvas) -> None:
